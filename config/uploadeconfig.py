ca13636f0e6010b020373284da815838c2e5e98a
terajh95 <terajoohyun@ajou.ac.kr>
2020-09-21 00:38:45 +0900

    refactoring codes...

---
+++ config/uploadeconfig.py
@@ -0,0 +1,10 @@
+
+
+UPLOAD_FOLDER = '../statics/test_img/'
+UPLOAD_ROI = '../statics/roid/'
+UPLOAD_TW3 = '../statics/tw3_roi/'
+CLIENT_IMAGES= '/home/ubuntu/Desktop/dev/Papaya-master/statics/test_img/'
+MODAL= './static/Uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'bmp'])
+DCM_EXTENSIONS = set(['dcm'])
+# key = 'why would I tell you my secret key?'
