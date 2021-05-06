3134dc8e2f598a64d16e0d42ebf714d490cbfd9b
ParkJuHyeon <terajoohyun@ajou.ac.kr>
2021-05-06 13:24:07 +0900

    배포용 코드 작성중

--- start.sh
+++
@@ -1,3 +0,0 @@
-export FLASK_APP=nb
-export FLASK_ENV=development
-
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# 파이썬 3.7 설치
-sudo apt update
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.7
